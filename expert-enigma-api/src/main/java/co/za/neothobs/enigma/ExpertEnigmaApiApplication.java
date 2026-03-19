de6f506791107b2a77b392ebf80fbf6e035a6f6b
neoThobs <neo.thobz@gmail.com>
2026-03-19 18:39:08 +0200

    feat: Add api project

---
+++ expert-enigma-api/src/main/java/co/za/neothobs/enigma/ExpertEnigmaApiApplication.java
@@ -0,0 +1,13 @@
+package co.za.neothobs.enigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertEnigmaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertEnigmaApiApplication.class, args);
+    }
+
+}
---
+++ expert-enigma-api/src/test/java/co/za/neothobs/enigma/ExpertEnigmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.neothobs.enigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertEnigmaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
